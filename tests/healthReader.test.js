2630911313e678fee4f80bc0ac79a878812eb346
Nicholas Williams <npwilliams@cru.umhb.edu>
2026-02-08 10:30:53 -0600

    Completed fitness data processor assignment

--- tests/healthReader.test.js
+++ tests/healthReader.test.js
@@ -1,7 +1,8 @@
 const { healthMetricsCounter } = require("./healthReader");
 
 test("reads valid JSON file", async () => {
-  const result = await healthMetricsCounter("./data/health.json");
+  const result = await healthMetricsCounter("./data/health-metrics.json");
+
   expect(result.totalEntries).toBeGreaterThan(0);
 });
 
